da1c31d3b9262686431f6957875c755aa0b0e3bf
Dmitriy Dultsev <tsukikohaya@gmail.com>
2025-08-08 12:20:45 +0300

    realize mock auth

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,20 @@
 import { Outlet } from "react-router";
+import styles from "./layout.module.scss";
 
 const Layout = () => (
-  <div className="h-dvh relative bg-slate-600">
+  <div className={styles.background}>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
     <Outlet />
   </div>
 );
