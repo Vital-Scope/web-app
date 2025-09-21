3446594084f1409300b584c491c7eab0a4ef8bb5
Dmitriy <tsukikohaya@gmail.com>
2025-09-21 14:54:59 +0300

    upd

--- src/pages/patients/form/formSchema.ts
+++ src/pages/patients/form/formSchema.ts
@@ -3,15 +3,15 @@ import { z } from "zod";
 const formSchema = z.object({
   lastName: z.string().min(1, "Фамилия обязательна"),
   firstName: z.string().min(1, "Имя обязательно"),
-  middleName: z.string().optional(),
+  middleName: z.string().nullable(),
   birthDate: z.number("Дата рождения обязательна"), // timestamp
-  clientId: z.string().optional(),
-  pregnancyWeek: z.number().min(0).max(45).optional().nullable(),
-  pregnancyNumber: z.number().min(1).optional().nullable(),
+  clientId: z.string().nullable(), // id интеграции
+  pregnancyWeek: z.number().min(0).max(45).nullable(), // неделя беременности
+  pregnancyNumber: z.number().min(1).nullable(), // какая по счету беременность
   dueDate: z.number().optional().nullable(), // timestamp
-  anamnesis: z.string().optional(),
-  doctorNotes: z.string().optional(),
-  avatar: z.string().optional().nullable(),
+  anamnesis: z.string(),
+  doctorNotes: z.string(),
+  avatar: z.string().nullable(),
 });
 
 export default formSchema;
